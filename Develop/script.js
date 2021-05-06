4a2ddf013625e408f7fd392cbd15da321a237075
Hunter Johnson <hunterjohnsoninfo@gmail.com>
2021-05-06 18:46:32 -0500

    Went through acceptance criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,53 @@
 // Assignment code here
 
-
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numChar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const writePassword = () => {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
+const number = 5
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const generatePassword = () => {
+  const valueRange = prompt("Select a value between 8-128 as the length of password")
+
+  const includesLowCase = confirm("Do you want to have lower case letters?");
+  const includesUpCase = confirm("Do you want to have upper case letters?");
+  const includesNum = confirm("Do you want to have numbers in your password?");
+  const includeSpChar = confirm("Do you want to have special characters?");
+
+
+  let chosenCharacters = [];
+  // evaluate your input
+  if (includeSpChar) {
+    chosenCharacters = chosenCharacters.concat(specialCharacters)
+  }
+  if (includesNum) {
+    chosenCharacters = chosenCharacters.concat(numChar)
+  }
+  if (includesUpCase) {
+    chosenCharacters = chosenCharacters.concat(upperCase)
+  }
+  if (includesLowCase) {
+    chosenCharacters = chosenCharacters.concat(lowerCase)
+  }
+
+  let PasswordVariable = ""
+
+  for (let i = 0; i < parseInt(valueRange); ++i) {
+    PasswordVariable += chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)]
+  }
+
+  return PasswordVariable
+}
